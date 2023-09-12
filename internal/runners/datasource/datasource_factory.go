aa7e7b1b9c0ff554be46b7791e7a6623c6d712b1
Aliaksandr Shcherba <salsarodrigues@icloud.com>
2023-09-12 19:20:07 +0300

    feat: add runner interface, csv data source, data source factory. Update readme.md

---
+++ internal/runners/datasource/datasource_factory.go
@@ -0,0 +1,40 @@
+package datasource
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	cnst "playground/internal/constants"
+	"playground/internal/runners/common"
+	"playground/internal/runners/datasource/csv"
+	t "playground/internal/types"
+	"playground/internal/utils/cerror"
+	"sync"
+)
+
+// NewDataSource creates a new data source for processing
+func NewDataSource(
+	ctx context.Context,
+	wg *sync.WaitGroup,
+	filePath string,
+	recordCh t.RecordChannel,
+	errorCh t.ErrorChannel) (common.IRunner, error) {
+
+	// Check for file exists
+	_, err := os.Stat(filePath)
+	if err != nil {
+		return nil, cerror.NewCustomError(fmt.Sprintf("%q no such file", filePath))
+	}
+
+	// Get file extension
+	ext := filepath.Ext(filePath)
+
+	// General Factory logic, create data source depends on file extension
+	switch ext {
+	case cnst.CsvDataSource:
+		return csv.NewDataSource(ctx, wg, filePath, recordCh, errorCh)
+	default:
+		return nil, cerror.NewCustomError(fmt.Sprintf("%q invalid data source type extension", ext))
+	}
+}
