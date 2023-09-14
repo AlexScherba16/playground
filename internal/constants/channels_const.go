ba917932b967fdd8732d8d634002f14819db3751
Aliaksandr Shcherba <salsarodrigues@icloud.com>
2023-09-14 16:14:12 +0300

    feat: add postprocessor types and constants

--- internal/constants/channels_const.go
+++ internal/constants/channels_const.go
@@ -1,8 +1,9 @@
 package constants
 
 const (
-	RecordChannelBuffer    = 10
-	ErrorChannelBuffer     = 0
-	AggregateChannelBuffer = 5
-	PredictChannelBuffer   = 5
+	RecordChannelBuffer        = 10
+	ErrorChannelBuffer         = 0
+	AggregateChannelBuffer     = 5
+	PredictChannelBuffer       = 5
+	PostProcessorChannelBuffer = 5
 )
